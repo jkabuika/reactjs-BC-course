ac70fdfb36006f1c42569e87481366fc48e17203
jkabuika <jkabuika1319@gmail.com>
2025-02-09 08:31:01 -0500

    corrected errors

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card(){
+  return(
+    <div className="card">
+      <img></img>
+      <h2>Jonathan Kaciunga</h2>
+      <p>I love playing basketball</p>
+    </div>
+  )
+}
+
+export default Card
